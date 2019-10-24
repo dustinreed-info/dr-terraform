0d1da70b11012156b7e2ec16a322740c0a540a1a
dustinreed-info <dustin@dustinreed.info>
2019-10-24 02:19:23 -0500

    Misc update.

--- config.tpl
+++ config.tpl
@@ -5,8 +5,6 @@ sudo yum install git -y
 git clone https://github.com/dustinreed-info/dr-info-website-flask /tmp/dr-info
 sudo yum install python37 -y
 sudo pip3 install flask python-dotenv flask-wtf gunicorn
-cd /tmp/dr-info
-git checkout dev
 sudo cp /tmp/dr-info/drinfo.service /etc/systemd/system/drinfo.service
 sudo systemctl daemon-reload
 sudo systemctl start drinfo
--- vars.tf
+++ vars.tf
@@ -1,15 +1,14 @@
 variable "public_key_path" {
   description = "Path to Public Key"
-  #   default     = "~/.ssh/terraform.pub"
+  default     = "~/.ssh/dr-tf-key.pub"
 }
 
 variable "key_name" {
   description = "Desired name of AWS key pair"
-  #   default     = "dr-tf-key"
+  default     = "dr-tf-key"
 }
 
 variable "aws_region" {
   description = "AWS region to launch servers."
   default     = "us-east-1"
 }
-
